3c885685c380236762c51fa1d415aec7745c297e
Jas <jason30102212@gmail.com>
2019-02-06 16:24:29 +1100

    Initial Commit

---
+++ ABOUT.js
@@ -0,0 +1,87 @@
+// ABOUT THIS SITE
+
+// This sites sole purpose is to be a potfolio site.
+
+
+// TECHNOLOGIES TO USE:
+//  Minimal technologies should be used in order to build the site.
+//  Frontend:
+//  - React
+//  - Javascript/ Jquery
+//  - HTML
+//
+//  Backend:
+//
+//  Other:
+//  - GIT
+//  - Heroku (CLI/ toolbelt)
+
+// TECHNOLOGIES TO AVOID:
+//  Avoid these technologies in order to not get side-tracked. Demonstrations
+//  of these technologies should be linked to outter projects.
+//  - Redux
+//  - MongoDB
+
+
+
+//  SCRUM:
+//
+//  Product Backlog (Prioratised list of features)
+//  - 2/6/2019:
+//    -- Get app online. Priority 1.
+//    -- Top navigation bar component. Priority 2.
+//    -- Header introduction display component(Landing). Priority Priority 2.
+//    -- Footer. Priority 2.
+//    -- Current project display. Priority 3.
+//    - Skillset. Priority 3.
+//    - About. Priority 3.
+//    - Completed Projects. Priority 4.
+//    - About this site. Priority 4.
+//    - Github code. Priority 4.
+//    - Autoscroll Functionality. Priority 5.
+
+//  User stories (As a ___, I need___, So that___)
+//    - 2/6/2019
+//      - Get app online. Priority 1.
+//        - As a site visitor, i need to be able to access this site from the internet, so that i can view its content
+//      - Top navigation bar component. Priority 2.
+//        - As a site visitor, i need to be able to navigate the website, so that I can find the content I am looking for.
+//      - Header introduction display component(Landing). Priority Priority 2.
+//        - As a site visitor, i need to have some context as to what this site is about and what it contains, so that i can decide if i want to continue.
+//    - Footer. Priority 2.
+//        - As a site visitor, i need to know where the page ends and my options to continue are, so that i can decide my next action.
+//    - Current project display. Priority 3.
+//        - As a site visitor, i need to be able to see a list of current projects that the creator is working on, so i can decide if i want to view their progress.
+//    - Skillset. Priority 3.
+//        - As a site visitor, i need to be able to view the creators current skillset, so that i can decide if they meet the hireablility criteria.
+//    - About. Priority 3.
+//        - As a site visitor, i need to see details about the creator, so that i can determine if i wish to continue on the site.
+//    - Completed Projects. Priority 4.
+//        - As a site visitor, i need to see a list of completed projects, so that i can decide if they meet the hireablility criteria.
+//    - About this site. Priority 4.
+//        - As a site visitor, i need to know how this site was built, so i can decide on the hireablility of the creator.
+//    - Github code. Priority 4.
+//        - As a site visitor, i need to be able to view the github content created by the creator, so that i can determine their hireablility.
+//    - Autoscroll Functionality. Priority 5.
+//        - As a site visitor, i would like to be able to access content in a more stylish manner, so i have a good UX experience
+
+
+//    - Completed Projects. Priority 4.
+//        - As a site visitor, I
+
+// Sprint Backlog
+
+//  Sprint Planning
+//  Daily Scrum
+//  Sprint review
+
+
+// SPRINT LOG
+//  - 2/6/2019
+//    - Plan
+//      - 2/6/2019
+//        - Get app online. Priority 1.
+//            - Use heroku
+//    - Build
+//    - Test
+//    - Review
