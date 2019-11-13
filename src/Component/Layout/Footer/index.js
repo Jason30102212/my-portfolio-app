8802ec01646d2e1023fbb56502d58d61e1e888a1
Jas <jason30102212@gmail.com>
2019-11-13 15:00:33 +1100

    Updated footer

--- src/Component/Layout/Footer/index.js
+++ src/Component/Layout/Footer/index.js
@@ -12,17 +12,16 @@ class Footer extends Component {
             <div class="col-1-of-2">
                 <div class="footer__navigation">
                     <ul class="footer__list">
-                        <li class="footer__item"><a href="#" class="footer__link">Company</a></li>
-                        <li class="footer__item"><a href="#" class="footer__link">Contact us</a></li>
-                        <li class="footer__item"><a href="#" class="footer__link">Carrers</a></li>
-                        <li class="footer__item"><a href="#" class="footer__link">Privacy policy</a></li>
-                        <li class="footer__item"><a href="#" class="footer__link">Terms</a></li>
+                        <li class="footer__item"><a href="#" class="footer__link">Projects</a></li>
+                        <li class="footer__item"><a href="#" class="footer__link">MERN</a></li>
+                        <li class="footer__item"><a href="#" class="footer__link">About</a></li>
+                        <li class="footer__item"><a href="#" class="footer__link">Contact</a></li>
                     </ul>
                 </div>
             </div>
             <div class="col-1-of-2">
                 <p class="footer__copyright">
-                    Footer
+                    Jason@jasonsonlineresume.com
                 </p>
             </div>
         </div>
