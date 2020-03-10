ae707f0f725478897244922537b59e6c23b133c3
Jas <jason30102212@gmail.com>
2020-03-10 12:18:23 +1100

    Update

--- src/Component/Layout/Header/index.js
+++ src/Component/Layout/Header/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import '../Layout.css'
-import {ReactComponent as Puzzle0} from '../../../images/puzzles/puzzle-mern.svg'
-import {ReactComponent as Puzzle1} from '../../../images/puzzles/puzzle-mongodb.svg'
-import {ReactComponent as Puzzle2} from '../../../images/puzzles/puzzle-express.svg'
-import {ReactComponent as Puzzle3} from '../../../images/puzzles/puzzle-react.svg'
-import {ReactComponent as Puzzle4} from '../../../images/puzzles/puzzle-node.svg'
-import {ReactComponent as Puzzle5} from '../../../images/puzzles/puzzle-redux.svg'
-import {ReactComponent as Puzzle6} from '../../../images/puzzles/puzzle-play.svg'
+// import {ReactComponent as Puzzle0} from '../../../images/puzzles/puzzle-mern.svg'
+// import {ReactComponent as Puzzle1} from '../../../images/puzzles/puzzle-mongodb.svg'
+// import {ReactComponent as Puzzle2} from '../../../images/puzzles/puzzle-express.svg'
+// import {ReactComponent as Puzzle3} from '../../../images/puzzles/puzzle-react.svg'
+// import {ReactComponent as Puzzle4} from '../../../images/puzzles/puzzle-node.svg'
+// import {ReactComponent as Puzzle5} from '../../../images/puzzles/puzzle-redux.svg'
+// import {ReactComponent as Puzzle6} from '../../../images/puzzles/puzzle-play.svg'
+import mongodbLogo from '../../../images/mongodb-logo.png'
+import expressLogo from '../../../images/express-logo.png'
+import reactLogo from '../../../images/react-logo.png'
+import javascriptLogo from '../../../images/javascript-logo.png'
+
 
 class Header extends Component {
   render () {
@@ -28,17 +33,29 @@ class Header extends Component {
           <h1 className="heading-primary">
             <span className="heading-primary--main">Jason's Portfolio</span>
             <span className="heading-primary--sub">MERN app development</span>
+            {
+            // <div className="heading-primary__logo">
+            //   <img className="heading-primary__logo--express" src={mongodbLogo} alt="logo"></img>
+            //   <img className="heading-primary__logo--express" src={expressLogo} alt="logo"></img>
+            //   <img className="heading-primary__logo--express" src={reactLogo} alt="logo"></img>
+            //   <img className="heading-primary__logo--express" src={javascriptLogo} alt="logo"></img>
+            // </div>
+            }
           </h1>
         </div>
-        <div className="header__puzzle-box">
-          <Puzzle0 className="header__puzzle-piece header__puzzle-piece--0 puzzle-shadow" />
-          <Puzzle1 className="header__puzzle-piece header__puzzle-piece--1 puzzle-shadow" />
-          <Puzzle2 className="header__puzzle-piece header__puzzle-piece--2 puzzle-shadow" />
-          <Puzzle3 className="header__puzzle-piece header__puzzle-piece--3 puzzle-shadow" />
-          <Puzzle4 className="header__puzzle-piece header__puzzle-piece--4 puzzle-shadow" />
-          <Puzzle5 className="header__puzzle-piece header__puzzle-piece--5 puzzle-shadow" />
-          <Puzzle6 className="header__puzzle-piece header__puzzle-piece--6 puzzle-shadow" />
-        </div>
+        {
+
+          // <div className="header__puzzle-box">
+          //   <Puzzle0 className="header__puzzle-piece header__puzzle-piece--0 puzzle-shadow" />
+          //   <Puzzle1 className="header__puzzle-piece header__puzzle-piece--1 puzzle-shadow" />
+          //   <Puzzle2 className="header__puzzle-piece header__puzzle-piece--2 puzzle-shadow" />
+          //   <Puzzle3 className="header__puzzle-piece header__puzzle-piece--3 puzzle-shadow" />
+          //   <Puzzle4 className="header__puzzle-piece header__puzzle-piece--4 puzzle-shadow" />
+          //   <Puzzle5 className="header__puzzle-piece header__puzzle-piece--5 puzzle-shadow" />
+          //   <Puzzle6 className="header__puzzle-piece header__puzzle-piece--6 puzzle-shadow" />
+          // </div>
+
+        }
       </header>
     )
   }
